0c4f1e5d0f74cc41d32e3b065b8e393d0a54df22
Marcelo <marceloeduardo244@gmail.com>
2020-06-20 17:22:03 -0300

    parte visual ok

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu cadastro</h1>
+
+        <Input icon={FiUser} name="name" placeholder="Nome" />
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
+
+        <Input
+          icon={FiLock}
+          name="password"
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Entrar</Button>
+      </form>
+
+      <a href="login">
+        <FiArrowLeft />
+        Voltar para o logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
